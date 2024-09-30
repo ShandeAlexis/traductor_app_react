0ae9d3aabe08be834686c8f67c8954de233cffca
Alexis Diaz <alexisshande@gmail.com>
2024-09-30 10:32:20 -0500

    Mini proyecto de traductor

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,68 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputText ,setInputText]= useState('');
+  const [translatedText, setTranslateText] = useState('');
 
+  const translateText = async () => {
+    const apiURL= "https://libretranslate.com/translate";
+    //@ts-ignore
+    const apikey= process.env.API_KEY;
+    try {
+      const response = await fetch(apiURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          q: inputText,
+          source: 'auto',
+          target: 'es',
+          alternatives: 3,
+          api_key: apikey,
+        }),
+      });
+
+      if(response.ok){
+        const data = await response.json();
+        setTranslateText(data.translatedText)
+      }else{
+        alert('Hubo un error al traducir el texto.')
+      }
+    } catch (error) {
+      alert('Ocurrio uno error al conectar la API')
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       <div className="app-container">
+      <h1 className="title">Traductor de Inglés a Español</h1>
+      <div className="translator-section">
+        <div className="text-area-section">
+          <div className="input-section">
+            <h2>Inglés</h2>
+            <textarea
+              className="input-text"
+              placeholder="Escribe aquí..."
+              value={inputText}
+              onChange={(e)=>setInputText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="output-section">
+            <h2>Español</h2>
+            <textarea
+              className="output-text"
+              disabled
+              value={translatedText}
+              placeholder="Traducción aquí..."
+            ></textarea>
+          </div>
+        </div>
+        <button className="translate-button" onClick={translateText}>Traducir</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
